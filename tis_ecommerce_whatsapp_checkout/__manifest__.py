0482fa4f04eff1fd30d28289e23fd66d6e69f8ce
Mario Nuñez <marionumza@gmail.com>
2020-08-20 02:04:31 -0300

    subida de modulo

---
+++ tis_ecommerce_whatsapp_checkout/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
+# - © Technaureus Info Solutions Pvt. Ltd 2020. All rights reserved.
+
+{
+    'name': 'eCommerce WhatsApp Checkout',
+    'version': '13.0.0.2',
+    'summary': 'eCommerce WhatsApp Checkout',
+    'sequence': 1,
+    'author': 'Technaureus Info Solutions Pvt.Ltd.',
+    'description': 'This module is for eCommerce WhatsApp Checkout',
+    'category': 'eCommerce',
+    'website': 'http://www.technaureus.com',
+    'depends': ['website_sale'],
+    'license': 'Other proprietary',
+    'price': 50,
+    'currency': 'EUR',
+    'data': [
+        'views/templates.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'images': ['images/main_screenshot.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
